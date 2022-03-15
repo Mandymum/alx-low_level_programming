95857fca08fb40d03e6d0795eb029f58166ffd07
Umemoro Faith Ogechi <umemorofaith@gmail.com>
2022-03-14 18:30:01 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main(void)
+{
+	char ch = 'a';
+		while(ch <= 'z')
+		{
+			if (ch != 'e' && ch != 'q')
+			{
+				putchar(ch);
+			}
+			ch++;
+		}
+		putchar('\n');
+
+return 0;
+}
+
+#include <stdio.h>
+
+{
+	char ch = 'a';
+       		while(ch <= 'z'; ch++)
+	{
+		if (ch == 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+return 0;
+}
+
+#include <stdio.h>
+{
+	char ch = 'a';
+       		while(ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		ch++;
+		putchar(ch)
+	}
+	putchar('\n');
+
+return 0;
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	if (i = 0; i <= 9; i++)
+	{
+		printf("%d");
+	}
+	printf("\n");
+
+return 0;
+}
